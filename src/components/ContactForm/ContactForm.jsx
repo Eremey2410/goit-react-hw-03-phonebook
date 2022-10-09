12e780c12f2b42b88e376f8c28648c77b6fa303d
Eremey2410 <2410esv@gmail.com>
2022-10-09 21:29:57 +0300

    add ContactForm,ContactList,Filter

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,65 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+
+class ContactForm extends Component {
+  numberId = nanoid();
+  nameId = nanoid();
+
+  state = { name: '', number: '' };
+
+  handleInputChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor={this.nameId}>
+          Name
+          <input
+            id={this.nameId}
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        <label htmlFor={this.numberId}>
+          Number
+          <input
+            id={this.numberId}
+            type="tel"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleInputChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+export default ContactForm;
